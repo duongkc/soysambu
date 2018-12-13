ac295d3c9423d21a3faa1c10b17a7c023e9f1af8
Niels van der Vegt <ns.vandervegt@gmail.com>
2018-12-13 22:14:48 +0100

    Added pagination using the navbar through JQuery and Ajax, page contents are now dynamically loaded in using external HTML files.

--- src/main/webapp/js/webapp.functionality.js
+++ src/main/webapp/js/webapp.functionality.js
@@ -1,11 +1,39 @@
 $(document).ready( function() {
-    // Function to make navbar adjust carousel header on navigation
-    $('#carousel-header').on('slide.bs.carousel', function(event) {
+    // Function to make navbar adjust carousel header on navigation.
+    $('#carousel-header').on('slide.bs.carousel', function(e) {
         // Get indexes of current and clicked nav items.
         var from = $('.nav-link.active').index();
-        var to = $(event.relatedTarget).index();
+        var to = $(e.relatedTarget).index();
         // Remove active class from previous nav item and add to clicked nav item.
         $('.nav-link').removeClass('active');
         $('.nav-link').eq(to).addClass('active');
+    });
+
+    /* --- Load GIS page when navigated to using AJAX. --- */
+    $('#nav-gis').click(function() {
+        // If already on the GIS page, don't continue.
+        if($('#nav-gis').children().hasClass('active')) return false;
+
+        // Fadeout current content page.
+        $('#content').fadeOut(175, function () {
+            // Empty content div; Load gis.html and fade into content div.
+            $('#content').empty().load("gis.html", function() {
+                $(this).fadeIn(175);
+            });
+        });
+    });
+
+    /* --- Load Add Record page when navigated to using AJAX. --- */
+    $('#nav-addrecords').click(function() {
+        // If already on the Add Record page, don't continue.
+        if($('#nav-addrecords').children().hasClass('active')) return false;
+
+        // Fadeout current content page.
+        $('#content').fadeOut(175, function () {
+            // Empty content div; Load addrecord.html and fade into content div.
+            $('#content').empty().load("addrecord.html", function() {
+                $(this).fadeIn(175);
+            });
+        });
     });
 });
