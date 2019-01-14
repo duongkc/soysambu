8661b469f9c08393480156ab93bfce8ff5b8b514
Niels van der Vegt <ns.vandervegt@gmail.com>
2019-01-14 14:31:09 +0100

    Added multivariate visualization for sighting points. Points now grow in size and change color based on giraffe count size

--- src/main/webapp/js/arcgis.js
+++ src/main/webapp/js/arcgis.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     ) {
         /*** Define map and view ***/
         var map = new Map({
-            basemap: "gray"
+            basemap: "topo"
         });
 
         var view = new MapView({
@@ -147,180 +147,198 @@ $(document).ready(function () {
             var renderer = {
                 type: "simple",
                 symbol: {
-                    type: "simple-marker",
-                    size: 6,
-                    color: "gray"
-                }
+                    type: "simple-marker"
+                },
+                visualVariables: [
+                    // Variable to adjust point color based on giraffe count.
+                    {
+                        type: "color",
+                        field: "count",
+                        stops: [
+                            { value: 1, color: "rgba(247, 215, 148,0.5)" },
+                            { value: 25, color: "rgba(241, 144, 102,0.5)" },
+                            { value: 50, color: "rgba(196, 69, 105,0.5)" }
+                        ]
+                    },
+                    // Variable to adjust point size based on giraffe count.
+                    {
+                        type: "size",
+                        field: "count",
+                        stops: [
+                            { value: 1, size: 4 },
+                            { value: 50, size: 50 }
+                        ]
+                    }]
             };
 
-            var giraffeLayer = new FeatureLayer({
-                source: giraffeFeatures.graphics,
-                fields: giraffeFeatures.fields,
-                objectIdField: "id",
-                renderer: renderer,
-                outFields: ["*"],
-                popupTemplate: {title: 'Giraffe Sighting',
-                    content: generateContent}
-            });
+        var giraffeLayer = new FeatureLayer({
+            source: giraffeFeatures.graphics,
+            fields: giraffeFeatures.fields,
+            objectIdField: "id",
+            renderer: renderer,
+            outFields: ["*"],
+            popupTemplate: {title: 'Giraffe Sighting',
+                content: generateContent}
+        });
 
-            // Add FeatureLayer to map.
-            map.add(giraffeLayer);
-        }
+        // Add FeatureLayer to map.
+        map.add(giraffeLayer);
+    }
 
 
-        /** Generates popup template content.
-         * @returns {HTMLObjectElement} content - HTML popup template content
-         */
-        function generateContent(feature) {
-            var attributes = feature.graphic.attributes;
+    /** Generates popup template content.
+     * @returns {HTMLObjectElement} content - HTML popup template content
+     */
+    function generateContent(feature) {
+        var attributes = feature.graphic.attributes;
 
-            // Content container.
-            var content = document.createElement('div');
-            // Horizontal breaks.
-            var hr = document.createElement('HR');
-            var hr2 = document.createElement('HR');
+        // Content container.
+        var content = document.createElement('div');
+        // Horizontal breaks.
+        var hr = document.createElement('HR');
+        var hr2 = document.createElement('HR');
 
-            // Generate contents.
-            var summary = generateAttributes(attributes);
-            var chart = generateChart(attributes);
+        // Generate contents.
+        var summary = generateAttributes(attributes);
+        var chart = generateChart(attributes);
 
-            content.append(summary);
-            content.append(hr);
-            content.append(chart);
+        content.append(summary);
+        content.append(hr);
+        content.append(chart);
 
-            return content;
-        }
+        return content;
+    }
 
-        /** Generates HTML containing the records' attributes.
-         * @param {object} attributes - the sighting attributes.
-         * @returns {HTMLObjectElement} row - HTML containing attributes attributes.
-         */
-        function generateAttributes(attributes) {
-            // Create skeleton of HTML Layout.
-            var row = document.createElement('div');
-            var col1 = document.createElement('div');
-            var col2 = document.createElement('div');
-            row.className = "row";
-            col1.className = "col-6";
-            col2.className = "col-6";
-            // Fill columns with attributes.
-            col1.innerHTML = "<span><b>Activity: </b>" + capitalizeFirstLetter(attributes.activity) +
-                "</span><br><span><b>Habitat: </b>" + capitalizeFirstLetter(attributes.habitat) +
-                "</span><br><span><b>Weather: </b>" + capitalizeFirstLetter(attributes.weather) +
-                "</span>";
+    /** Generates HTML containing the records' attributes.
+     * @param {object} attributes - the sighting attributes.
+     * @returns {HTMLObjectElement} row - HTML containing attributes attributes.
+     */
+    function generateAttributes(attributes) {
+        // Create skeleton of HTML Layout.
+        var row = document.createElement('div');
+        var col1 = document.createElement('div');
+        var col2 = document.createElement('div');
+        row.className = "row";
+        col1.className = "col-6";
+        col2.className = "col-6";
+        // Fill columns with attributes.
+        col1.innerHTML = "<spaattributesn><b>Activity: </b>" + capitalizeFirstLetter(attributes.activity) +
+            "</spaattributesn><br><span><b>Habitat: </b>" + capitalizeFirstLetter(attributes.habitat) +
+            "</span><br><span><b>Weather: </b>" + capitalizeFirstLetter(attributes.weather) +
+            "</span>";
 
-            col2.innerHTML = "<span><b>Longitude: </b>" + attributes.longitude +
-                "</span><br><span><b>Latitude: </b>" + attributes.latitude +
-                "</span>";
+        col2.innerHTML = "<span><b>Longitude: </b>" + attributes.longitude +
+            "</span><br><span><b>Latitude: </b>" + attributes.latitude +
+            "</span>";
 
-            row.appendChild(col1);
-            row.appendChild(col2);
+        row.appendChild(col1);
+        row.appendChild(col2);
 
-            return row
-        }
+        return row
+    }
 
-        /** Generates a stacked horizontal bar chart of the giraffe group composition using Chart.js.
-         * @param {object} attributes - the sighting attributes.
-         * @returns {HTMLObjectElement} row - HTML containing chart title and canvas.
-         */
-        function generateChart(attributes) {
-            // Create row container for chart title and canvas.
-            var row = document.createElement('div');
-            row.className = "row justify-content-center";
-            // Create and add title to row as span element.
-            row.innerHTML = '<span><b>Group Composition</b> ('
-                + attributes.count + ' Giraffes)<br>';
+    /** Generates a stacked horizontal bar chart of the giraffe group composition using Chart.js.
+     * @param {object} attributes - the sighting attributes.
+     * @returns {HTMLObjectElement} row - HTML containing chart title and canvas.
+     */
+    function generateChart(attributes) {
+        // Create row container for chart title and canvas.
+        var row = document.createElement('div');
+        row.className = "row justify-content-center";
+        // Create and add title to row as span element.
+        row.innerHTML = '<span><b>Group Composition</b> ('
+            + attributes.count + ' Giraffes)<br>';
 
-            // Create canvas for chart.
-            var canvas = document.createElement('canvas');
-            canvas.setAttribute('height', '100px');
+        // Create canvas for chart.
+        var canvas = document.createElement('canvas');
+        canvas.setAttribute('height', '100px');
 
-            // Create chart using Chart.js and attributes values.
-            var chart = new Chart(canvas, {
-                type: 'horizontalBar',
-                data: {
-                    labels: [""],
-                    datasets: [{
-                        label: 'Male - A (' + attributes.male_adult + ')' ,
-                        data: [attributes.male_adult],
-                        backgroundColor: 'rgba(196, 69, 105,0.5)',
-                        borderColor: 'rgba(196, 69, 105,1)',
-                        borderWidth: 2
-                    }, {
-                        label: 'Male - SA (' + attributes.male_subadult + ')',
-                        data: [attributes.male_subadult],
-                        backgroundColor: pattern.draw('dash', 'rgba(207, 106, 135,0.5)')
-                    }, {
-                        label: 'Female - A (' + attributes.female_adult + ')',
-                        data: [attributes.female_adult],
-                        backgroundColor: 'rgba(245, 205, 121,0.5)',
-                        borderColor: 'rgba(245, 205, 121,1)',
-                        borderWidth: 2
-                    }, {
-                        label: 'Female - SA (' + attributes.female_subadult + ')',
-                        data: [attributes.female_subadult],
-                        backgroundColor: pattern.draw('dash', 'rgba(247, 215, 148,0.5)')
-                    }, {
-                        label: 'Juvenile (' + attributes.juvenile + ')',
-                        data: [attributes.juvenile],
-                        backgroundColor: 'rgba(119, 139, 235,0.5)'
-                    }, {
-                        label: 'Unidentified (' + attributes.ungroup_identified + ')',
-                        data: [attributes.ungroup_identified],
-                        backgroundColor: 'rgba(89, 98, 117,0.5)'
+        // Create chart using Chart.js and attributes values.
+        var chart = new Chart(canvas, {
+            type: 'horizontalBar',
+            data: {
+                labels: [""],
+                datasets: [{
+                    label: 'Male - A (' + attributes.male_adult + ')' ,
+                    data: [attributes.male_adult],
+                    backgroundColor: 'rgba(196, 69, 105,0.5)',
+                    borderColor: 'rgba(196, 69, 105,1)',
+                    borderWidth: 2
+                }, {
+                    label: 'Male - SA (' + attributes.male_subadult + ')',
+                    data: [attributes.male_subadult],
+                    backgroundColor: pattern.draw('dash', 'rgba(207, 106, 135,0.5)')
+                }, {
+                    label: 'Female - A (' + attributes.female_adult + ')',
+                    data: [attributes.female_adult],
+                    backgroundColor: 'rgba(245, 205, 121,0.5)',
+                    borderColor: 'rgba(245, 205, 121,1)',
+                    borderWidth: 2
+                }, {
+                    label: 'Female - SA (' + attributes.female_subadult + ')',
+                    data: [attributes.female_subadult],
+                    backgroundColor: pattern.draw('dash', 'rgba(247, 215, 148,0.5)')
+                }, {
+                    label: 'Juvenile (' + attributes.juvenile + ')',
+                    data: [attributes.juvenile],
+                    backgroundColor: 'rgba(119, 139, 235,0.5)'
+                }, {
+                    label: 'Unidentified (' + attributes.ungroup_identified + ')',
+                    data: [attributes.ungroup_identified],
+                    backgroundColor: 'rgba(89, 98, 117,0.5)'
+                }]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        ticks: {
+                            Min: 0,
+                            Max: attributes.count,
+                            stepSize: 1,
+                            maxTicksLimit: 10},
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        stacked: true
                     }]
-                },
-                options: {
-                    scales: {
-                        xAxes: [{
-                            ticks: {
-                                Min: 0,
-                                Max: attributes.count,
-                                stepSize: 1,
-                                maxTicksLimit: 10},
-                            stacked: true
-                        }],
-                        yAxes: [{
-                            stacked: true
-                        }]
-                    }
                 }
-            });
-
-            row.appendChild(canvas);
-
-            return row;
-        }
-
-        /** Takes a string, converts it to lower case and makes the first letter uppercase.
-         * @param {string} string - the string to be processed.
-         * @returns {string} returns either a successfully processed string or "-".
-         */
-        function capitalizeFirstLetter(string) {
-            if (string) {
-                string = string.toLowerCase();
-                // Capitalize first character, then add rest of lowercase string.
-                string = string.charAt(0).toUpperCase() + string.slice(1);
-                return string
-            } else {
-                // When the given parameter is not a string, return "-".
-                return "-"
             }
-        }
+        });
 
-        /** Removes the seconds from the records' time attribute.
-         * @param {object} time - the sighting time attribute.
-         * @returns {string} time - the records time attribute as a string with seconds removed.
-         */
-        function timeFormat(time) {
-            if (time) {
-                // Remove ':00'  from time counter.
-                time = time.slice(0, -3);
-                return time
-            } else {
-                // When the given parameter is not valid, return "-".
-                return "-"
-            }
+        row.appendChild(canvas);
+
+        return row;
+    }
+
+    /** Takes a string, converts it to lower case and makes the first letter uppercase.
+     * @param {string} string - the string to be processed.
+     * @returns {string} returns either a successfully processed string or "-".
+     */
+    function capitalizeFirstLetter(string) {
+        if (string) {
+            string = string.toLowerCase();
+            // Capitalize first character, then add rest of lowercase string.
+            string = string.charAt(0).toUpperCase() + string.slice(1);
+            return string
+        } else {
+            // When the given parameter is not a string, return "-".
+            return "-"
         }
-    });
+    }
+
+    /** Removes the seconds from the records' time attribute.
+     * @param {object} time - the sighting time attribute.
+     * @returns {string} time - the records time attribute as a string with seconds removed.
+     */
+    function timeFormat(time) {
+        if (time) {
+            // Remove ':00'  from time counter.
+            time = time.slice(0, -3);
+            return time
+        } else {
+            // When the given parameter is not valid, return "-".
+            return "-"
+        }
+    }
+});
 });
