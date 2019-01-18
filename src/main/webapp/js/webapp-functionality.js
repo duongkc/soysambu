9471d992b9b1be9ab81204ef5da595f7c780c6f8
Niels van der Vegt <ns.vandervegt@gmail.com>
2019-01-18 16:53:37 +0100

    Optimized webapp-functionality.js, updating it to EMCAScript 6; improved documentation for webapp-functionality

--- src/main/webapp/js/webapp-functionality.js
+++ src/main/webapp/js/webapp-functionality.js
@@ -1,13 +1,17 @@
 $(document).ready( function() {
-    /* Header responsiveness to screen size */
-    // Initial header set depending on screen size.
-    if (window.innerWidth < 750) { $("#carousel-header").hide(); }
+    /*** RESPONSIVE HEADER ***/
+    /* Initial header set depending on screen size. */
+    if (window.innerWidth < 750) {
+        $("#carousel-header").hide();
+    }
 
-    // Remove bootstrap's hidden styling on the headers.
-    // From here on this is handled by JS through the checkWidth function.
+    /* Remove bootstrap's initial hidden styling on the headers.
+       From here on this is handled by JS through the checkWidth function. */
     $('#gis-header').removeClass('d-none d-sm-block');
 
-    // Shows the appropriate header for screen size.
+    /** Function that checks if the user is on the GIS page and if the user is on
+     *  a small screen, then shows or hides the header carousel accordingly.
+     */
     function checkWidth() {
         if (window.innerWidth < 750 && $('#gis-header').hasClass('active')) {
             $("#carousel-header").slideUp()
@@ -16,62 +20,61 @@ $(document).ready( function() {
         }
     }
 
-    // Throttle the amount of times checkWidth is called when resize is thrown.
-    var throttled = _.throttle(checkWidth, 100, {leading: false});
+    /* Throttle the amount of times checkWidth is called when resize event is fired. */
+    const throttled = _.throttle(checkWidth, 100, {leading: false});
     $(window).resize(throttled);
 
-    // Function to fade in content div and footer and enable tooltips.
-    function showContent() {
-        $('#carousel-header').slideDown(function() {
-            $('#content, #footer').fadeIn(175, queu = false);
-        });
-        $('[data-toggle="tooltip"]').tooltip();
-    }
 
-    /* --- Make navbar adjust carousel header on navigation. --- */
-    $('#carousel-header').on('slide.bs.carousel', function(e) {
-        // Get indexes of current and clicked nav items.
-        var from = $('.nav-item.active').index();
-        var to = $(e.relatedTarget).index();
-        // Remove active class from previous nav item and add to clicked nav item.
+    /*** NAVBAR ***/
+    /* Make appropriate navbar item active depending on carousel sliding. */
+    $('#carousel-header').on('slide.bs.carousel', function(event) {
+        /* Get indexes of current and clicked nav items. */
+        let from = $('.nav-item.active').index();
+        let to = $(event.relatedTarget).index();
+
+        /* Remove active class from previous nav item and add to clicked nav item. */
         $('.nav-item').eq(from).removeClass('active');
         $('.nav-item').eq(to).addClass('active');
     });
 
-    /* --- Load GIS page when navigated to using AJAX. --- */
+
+    /*** DYNAMIC PAGE CONTENTS ***/
+    /** Function that fades in content and footer divs and enables tooltips on the current page. */
+    function showContent() {
+        /* Slide down carousel in case a small-screen user is coming from the GIS page */
+        $('#carousel-header').slideDown(function() {
+            $('#content, #footer').fadeIn({duration:175, queue: false});
+        });
+        $('[data-toggle="tooltip"]').tooltip();
+    }
+
+    /* Dynamically load GIS page when navigated to using AJAX. */
     $('#nav-gis').click(function() {
-        // If already on the GIS page, don't continue.
+        /* If already on the GIS page, don't continue. */
         if($('#nav-gis').hasClass('active')) return false;
 
-        // Fadeout current content page.
+        /* Fadeout current content. */
         $('#content, #footer').fadeOut(175, function () {
-            // Slide up carousel header when on small screen.
-            if (window.innerWidth < 750) { $("#carousel-header").slideUp() }
+            /* Slide up header carousel when on small screen. */
+            if (window.innerWidth < 750) {
+                $("#carousel-header").slideUp()
+            }
             $('#map').fadeIn(175);
         });
     });
 
-    /* --- Load Add Record page when navigated to using AJAX. --- */
-    $('#nav-addrecord').click(function() {
-        // If already on the Add Record page, don't continue.
-        if($('#nav-addrecord').hasClass('active')) return false;
+    /* Dynamically load other pages when navigated to using AJAX. */
+    $('#nav-addrecord, #nav-viewrecords').click(function() {
+        /* If already on the current page, don't continue. */
+        if($(this).hasClass('active')) return false;
+        /* Get target page from <li>'s data-page attribute */
+        const page = $(this).attr("data-page");
 
-        // Fadeout current content page.
+        /* Fadeout current content. */
         $('#content, #map').fadeOut(175).promise().done(function () {
-            // Empty content div; Load addrecord.html and fade into content div.
-            $('#content').empty().load("addrecord.html", showContent);
-        });
-    });
-
-    /* --- Load View Records page when navigated to using AJAX. --- */
-    $('#nav-viewrecords').click(function() {
-        // If already on the Add Record page, don't continue.
-        if($('#nav-viewrecords').hasClass('active')) return false;
-
-        // Fadeout current content page.
-        $('#content, #map').fadeOut(175).promise().done(function () {
-            // Empty content div; Load viewrecords.html and fade into content div.
-            $('#content').empty().load("viewrecords.html", showContent);
+            /* Empty content div.
+               Load appropriate page and fade into content div. */
+            $('#content').empty().load("" + page, showContent);
         });
     });
 });
